8e0d831ae77d72784f7f7a2a460a77272ed3435a
unknown <mkshah5@ncsu.edu>
2022-09-01 10:42:48 -0400

    NVCOMP decompress

--- preprocess/threshold.cu
+++ preprocess/threshold.cu
@@ -673,9 +673,9 @@ int main(int argc, char* argv[]){
             
             nvcompType_t data_type = NVCOMP_TYPE_CHAR;
 
-            LZ4Manager nvcomp_manager{chunk_size, data_type, stream};
+            auto  decomp_manager = =create_manager(d_comp, stream);
 
-            DecompressionConfig decomp_config = nvcomp_manager.configure_decompression((uint8_t *)d_comp);
+            DecompressionConfig decomp_config = decomp_manager->configure_decompression((uint8_t *)d_comp);
             
             #ifdef TIMING
             float time_NVCOMP;
